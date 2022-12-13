0c93fe162024aee1343f61e5d3a2151c31f38bba
David Velazquez <david.velazquez@blueoptima.com>
2022-12-12 20:17:34 -0600

    Removed unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import CalendarDay from "./components/CalendarDay";
-import useCalendar from "./hooks/calendar/useCalendar";
-import {useState} from "react";
 import Calendar from "./calendar";
 import './App.css'
 
 function App() {
 
-  const [date, setDate] = useState(new Date())
-
   return (
     <div className="App" >
-      <Calendar date={date} />
+      <Calendar date={new Date()} />
     </div>
   )
 }
