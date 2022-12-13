f54e6879aa2b0779aadac4a5019a09dcd95834bc
David Velazquez <david.velazquez@blueoptima.com>
2022-12-12 21:52:35 -0600

    Start navigation styles

--- src/components/CalendarNavigation/index.jsx
+++ src/components/CalendarNavigation/index.jsx
@@ -1,4 +1,4 @@
-import './calendarNavigation.scss'
+import "./calendarNavigation.scss";
 
 /**
  * Description: CalendarNavigation Component
@@ -11,9 +11,17 @@ import './calendarNavigation.scss'
 function CalendarNavigation({ getPrevMonth, getNextMonth, calendarTitle }) {
   return (
     <section className="calendar__navigation">
-      <span className="calendar__navigation__title" >{calendarTitle}</span>
-      <button onClick={getPrevMonth}> Prev Month </button>
-      <button onClick={getNextMonth}> Next Month </button>
+      <span className="calendar__navigation__title">{calendarTitle}</span>
+
+      <div className="calendar__navigation__arrows">
+        <button onClick={getPrevMonth}> Prev Month </button>
+        <button onClick={getNextMonth}> Next Month </button>
+      </div>
+
+      <div className="calendar__navigation__buttons">
+          <button className="calendar__navigation__buttons__today" >Today</button>
+          <button>time zones</button>
+      </div>
     </section>
   );
 }
