57f067706cfabf84a5973c053deb230054f00f94
David Velazquez <david.velazquez@blueoptima.com>
2022-12-13 20:17:25 -0600

    Add modal structure and overlay and body styles

---
+++ src/components/EventModal/index.jsx
@@ -0,0 +1,9 @@
+import './eventModal.scss'
+
+function EventModal () {
+    return <section className="calendar__modal calendar__modal__overlay">
+        <div className="calendar__modal--body"></div>
+    </section>
+}
+
+export default EventModal
